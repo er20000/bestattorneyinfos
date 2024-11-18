d7cfb4769d84bc8df3f43d586acd610923b67457
er20000 <eleazardropshipping@gmail.com>
2024-11-17 17:20:09 -0800

    Update src/main.tsx [BoltSync]

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
 
+// Handle redirects from 404.html
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, '', redirect);
+}
+
 const root = document.getElementById('root');
 
 if (root) {
